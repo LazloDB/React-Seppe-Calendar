a75820e3c181cc9dd0da382aa9c0692b63118773
Lazlo De Bock <lazlo.debock@novemberfive.co>
2019-03-04 09:29:58 +0100

    functional component day

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Container } from './Day.style';
 
-class Day extends React.Component {
-  render() {
-    const { day, routine, isToday, number } = this.props;
-
-    return (
-      <Container isFree={routine === 'X'} isToday={isToday}>
-        <div>{number}</div>
-        <div>{day}</div>
-        <div>{routine}</div>
-      </Container>
-    );
-  }
-}
+const Day = ({ day, routine, isToday, number }) => {
+  return (
+    <Container isFree={routine === 'X'} isToday={isToday}>
+      <div>{number}</div>
+      <div>{day}</div>
+      <div>{routine}</div>
+    </Container>
+  );
+};
 
 export default Day;
